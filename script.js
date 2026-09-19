266da86c01ff9aff694608fab1f0177a03399bca
Surendra906 <surendramode945@gmail.com>
2026-09-19 20:44:43 +0530

    Add functions to manipulate employee array

--- script.js
+++ script.js
@@ -7,21 +7,43 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
+  arr.map((employee)=>{
+    if(employee.profession==="developer"){
+      console.log(employee));
+    }
+  });
   //Write your code here , just console.log
 }
 
 function PrintDeveloperbyForEach() {
+  arr.forEach((employee)=>{
+    if(employee.profession==="developer"){
+      console.log(employee);
+    }
+  });
   //Write your code here , just console.log
 }
 
 function addData() {
+  let newEmployee={id:4,name:"susan",age:20,profession:"intern"};
+  arr.push(newEmployee);
+  console.log(newEmployee);
   //Write your code here, just console.log
 }
 
 function removeAdmin() {
+  let filteredArr=arr.filter((employee)=>employee.profession!=="admin");
+  console.log(filteredArr);
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
+  let newArr=[
+    {id:5,name:"alex",age:"23",profession:"designer"},
+    {id:6,name:"emma",age:"25",profession:"manager"},
+    {id:7,name:"liam",age:"21",profession:"tester"},
+  ];
+  let combineArray=arr.concat(newArr);
+  console.log(combinedArray);
   //Write your code here, just console.log
 }
